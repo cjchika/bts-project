8fd532c44390a5b5a588ed2b7609e7606c3b0683
Chijioke <8pluscreative@gmail.com>
2022-01-13 01:04:50 -0800

    Updating files...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 import Button from './components/UI/Button/Button'
 import './App.css';
 
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={showParagraph}/>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
