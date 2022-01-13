52d5cbb17939a099726ba3d5028933887f4068e8
Chijioke <8pluscreative@gmail.com>
2022-01-13 01:52:33 -0800

    Updating...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 import Button from './components/UI/Button/Button'
@@ -9,14 +9,14 @@ function App() {
 
   console.log('App Running...');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  }
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph}/>
+      <DemoOutput show={false}/>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
